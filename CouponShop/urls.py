281168c1dd71f21c9730f1dae33a1b64cbfa3d0c
Sajia09 <sajiaa09@gmail.com>
2023-04-04 13:25:39 +0600

    deployment

--- CouponShop/urls.py
+++ CouponShop/urls.py
@@ -24,6 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('coupon/', include('coupon.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
